c0982bd4ba70e46f3643f0e4f4eee280962702e6
Alex Dacre <alex@alexdacre.com>
2018-02-12 00:18:50 +1100

    Started building routes

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from 'react-apollo'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { MuiThemeProvider } from 'material-ui/styles'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import light from '../theme/light'
 import PokeList from '../Layouts/PokeList'
@@ -18,7 +19,12 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <MuiThemeProvider theme={light}>
-          <PokeList />
+          <Router>
+            <div>
+              <Route exact path='/' component={PokeList} />
+              <Route path='/poke' component={PokeList} />
+            </div>
+          </Router>
         </MuiThemeProvider>
       </ApolloProvider>
     )
---
+++ src/Layouts/PokeForm/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PokeForm = () => (
+  <div>
+    hi
+  </div>
+)
+
+export default PokeForm
